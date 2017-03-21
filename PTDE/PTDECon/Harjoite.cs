17a481be00b6b8963bf599f71318bd9676cfb7f5
reapercage <h4026@student.jamk.fi>
2017-03-21 22:34:45 +0200

    Alkua

---
+++ PTDE/PTDECon/Harjoite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTDECon
+{
+    class Harjoite
+    {
+        private int paivacounter;
+        private bool[] jako;
+        private double painojenMuutosD;
+        private double painojenMuutosSarja;
+    }
+    class Sarja
+    {
+        private double lahtopainot;
+        private double painot;
+        private int toistot;
+    }
+}
---
+++ PTDE/PTDECon/Harjoitusohjelma.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTDECon
+{
+    class Harjoitusohjelma
+    {
+        private DateTime aloituspaiva;
+        private string nimi;
+        private string currentTrainingDay;
+        private string currentTrainingWeek;
+        private int monijakoinen;
+        private List<Harjoite> harjoite;
+
+        public int Monijakoinen
+        {
+            get;
+        }
+    }
+}
